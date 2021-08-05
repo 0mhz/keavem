32ea6c1fc72714205330470e0725561646ad61a7
Laurent Ludwig <laurent.ludwig@e.email>
2021-08-05 16:04:47 +0200

    Added classes for DataCollection and FileHeader and their respective tags

--- parsesyn.py
+++ parsesyn.py
@@ -1,5 +1,14 @@
 """
-    This module parses the ASN.1 format syntax for counter data
+    This module parses the ASN.1 format syntax for counter data based on
+    decoded header definition
 """
 
 from etypes import *
+
+
+class DefinitionType:
+    pass
+
+
+class DefG18Q4:
+    pass
