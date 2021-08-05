026aca2474385742ed259452a22293282fd806ca
Laurent Ludwig <laurent.ludwig@e.email>
2021-08-05 16:04:47 +0200

    Added classes for DataCollection and FileHeader and their respective tags

--- etypes.py
+++ etypes.py
@@ -1,24 +1,102 @@
 """
-    This module contains the class definitions for the ASN.1 tags but will be
-    obsolete once an automatic parsing of the syntax has been implemented
+    This module contains the class definitions for the ASN.1 tags but will
+    (probably) be obsolete once an automatic parsing of the syntax has been
+    implemented. For now, this is used to decode the header in order to know
+    which specific sender type the BSC is using
 """
 
-from x690.types import Type
+from x690.types import GeneralizedTime, Integer, Null, Type, Utf8String
 from x690.util import TypeClass, TypeNature
 
 
-class MeasFileFooter(Type[bytes]):
-    TAG = 0x82
+class DataCollection(Type[bytes]):
+    # Sequence of: FileHeader, Sequence of MeasureData and FileFooter
+    TAG = 0x30
+    TYPECLASS = TypeClass.CONTEXT
+    NATURE = [TypeNature.CONSTRUCTED]
+
+
+class FileHeader(Type[bytes]):
+    # Sequence of:
+    # FormatVersion, Sendername, Sendertype, Vendorname, CollectionStartTime
+    # actually TAG = 0xa0 but 0x00 somehow yields a better result in decode()
+    TAG = 0x00
+    # DEC 160
+    TYPECLASS = TypeClass.CONTEXT
+    NATURE = [TypeNature.CONSTRUCTED]
+
+
+class FormatVersion(Type[Integer]):
+    TAG = 0x80
+    TYPECLASS = TypeClass.CONTEXT
+    NATURE = [TypeNature.PRIMITIVE]
+
+
+class SenderName(Type[Utf8String]):
+    TAG = 0x81
+    TYPECLASS = TypeClass.CONTEXT
+    NATURE = [TypeNature.PRIMITIVE]
+
+
+class SenderType(Type[str]):
+    # String length 0-8
+    # TAG = 0x82 Clash with FileFooter
+    TAG = Null
+    TYPECLASS = TypeClass.CONTEXT
+    NATURE = [TypeNature.PRIMITIVE]
+
+
+class VendorName(Type[str]):
+    # String length 0-32
+    TAG = 0x83
+    TYPECLASS = TypeClass.CONTEXT
+    NATURE = [TypeNature.PRIMITIVE]
+
+
+class LogStartTime(Type[str]):
+    # Actually a timestamp of type GeneralizedTime (presumably X.690 spec.)
+    TAG = 0x84
     TYPECLASS = TypeClass.CONTEXT
     NATURE = [TypeNature.PRIMITIVE]
 
     @staticmethod
     def decode_raw(data: bytes, slc: slice = slice(2, -1)) -> str:
         data = data[slc].decode("ascii")
-        return data
+        return GeneralizedTime(data)
+
+
+class MeasureData(Type[bytes]):
+    TAG = Null
+    TYPECLASS = TypeClass.CONTEXT
+    NATURE = [TypeNature.CONSTRUCTED]
+
+
+class FileFooter(Type[bytes]):
+    TAG = 0x82
+    TYPECLASS = TypeClass.CONTEXT
+    NATURE = [TypeNature.PRIMITIVE]
+
+    @staticmethod
+    def decode_raw(data: bytes, slc: slice = slice(None)) -> str:
+        return TimeStamp.get_time(data)
+
+    def use_gentime(data: bytes, slc: slice = slice(2, -1)) -> str:
+        data = data[slc].decode("ascii")
+        return GeneralizedTime(data)
+
+
+class TimeStamp(Type[bytes]):
+    TAG = Null
+    TYPECLASS = TypeClass.CONTEXT
+    NATURE = [TypeNature.PRIMITIVE]
 
     # This has been made in the middle of the night.
     # Probably decode once and return object as a tuple(time, timezone)
+    @staticmethod
+    def decode_raw(data: bytes, slc: slice = slice(2, -1)) -> str:
+        data = data[slc].decode("ascii")
+        return data
+
     def get_time(data: bytes, slc: slice = slice(2, 16)) -> str:
         data = data[slc].decode("ascii")
         return data
--- init.py
+++ init.py
@@ -6,10 +6,36 @@ from etypes import *
 file = "../asn_test_file.asn1"
 tag = "../e_measuredtime.asn1"
 """
-    tag containts a single MeasFileFooter tag (Type 130, 0x82):
-    82 0f 32 30 32 30 30 36  31 34 31 39 34 35 30 30   ..20200614194500
-    5a 0a                                              Z.
-    LengthInfo(length=15, offset=1)
+    The file measuredtime.asn1 contains a single FileFooter tag (0x82):
+        82 0f 32 30 32 30 30 36  31 34 31 39 34 35 30 30   ..20200614194500
+        5a 0a                                              Z.
+        LengthInfo(length=15, offset=1)
+"""
+header = "../e_fileheader.asn1"
+"""
+    The file fileheader.asn1 contains a FileHeader tag (0xa0) with length 0x39
+    (57 bytes) which contains the tags FormatVersion, Sendername, Sendertype,
+    Vendorname and CollectionStartTime.
+    FormatVersion [0x80, 0x01, 0x01] equals SOH (start of heading) in the
+    ASCII table which is weird as an Integer is expected
+
+    The Sendername begins with a tag 0x81 and is 23 bytes long
+    T81 L17 V[  42 53 43 47 41 52 45 2f 47
+                31 38 51 34 2f 30 33 20 20
+                20 20 20 20 20]
+    The Sendertype tag begins with 0x82 and always equals 1 in STS (proof required) so its length can
+    truncated; CLASH WITH FILEFOOTER 0x82
+
+    The LogStartTimestamp tag begins with 0x84
+    84 0f 32 30 32 30 30 36 31 34 31 39 33 30 30
+
+    The VendorName tag begins on 0x83 (length=8)
+
+        a0 39 80 01 01 81 17 42  53 43 47 41 52 45 2f 47   .9.....BSCGARE/G
+        31 38 51 34 2f 30 33 20  20 20 20 20 20 20 82 00   18Q4/03       ..
+        83 08 45 72 69 63 73 73  6f 6e 84 0f 32 30 32 30   ..Ericsson..2020
+        30 36 31 34 31 39 33 30  30                        061419300
+        LengthInfo(length=57, offset=1)
 """
 
 
@@ -27,11 +53,28 @@ def readTag():
     data = open(tag, "rb").read()
     print(visible_octets(data))
     print(decode_length(data, 1))
-    # decoded, nxt = decode(data)
     print(decode(data))
-    print(MeasFileFooter.decode_raw(data))
-    print(MeasFileFooter.get_time(data))
-    print(MeasFileFooter.get_timezone(data))
+
+    """
+        decoded, nxt = decode(data)
+        print(MeasFileFooter.decode_raw(data))
+        print(MeasFileFooter.get_time(data))
+        print(MeasFileFooter.get_timezone(data))
+        print(FileFooter.get_time(data))
+        print(FileFooter.get_timezone(data))
+    """
+
+    print(FileFooter.decode_raw(data))
+    print(FileFooter.use_gentime(data))
 
 
-readTag()
+def readHeader():
+    data = open(header, "rb").read()
+    print(visible_octets(data))
+    print(decode_length(data, 1))
+    # print(decode(data))
+
+
+# readTag()
+readFile()
+readHeader()
