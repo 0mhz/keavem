b3779b83feea0693cae8175b4870103b1307929d
Laurent Ludwig <laurent.ludwig@e.email>
2021-09-06 18:15:37 +0200

    Tinkering with GranularityPeriod, changed print output

--- entrypoint.py
+++ entrypoint.py
@@ -1,10 +1,12 @@
 from x690 import decode
 
-from keavem import decode as kvdecode
+from keavem import decode as keavem
+from keavem.structure import MeasDataCollection
 
 filename = "tests/data/file.asn1"
 with open(filename, "rb") as data:
     data = data.read()
 result, _ = decode(data)
-# print(result.pretty())
-print(result[1].pretty())
+meas_file_header, meas_data, meas_file_footer = result
+
+print(MeasDataCollection(meas_file_header, meas_data, meas_file_footer))
--- keavem/decode.unclean.py
+++ keavem/decode.unclean.py
@@ -8,13 +8,15 @@ from keavem.exceptions import DecodingUndefinedItemCount
 from keavem.structure import MeasData, MeasFileHeader, MeasInfo, MeasValue, NEId
 
 
-class ByteCodec(Type[bytes]):
+class ByteCodec(Type[bytes]):  # Isn't everything a byte codec here?
     TYPECLASS = TypeClass.CONTEXT
     NATURE = [TypeNature.PRIMITIVE]
     TAG = 0
 
     @staticmethod
     def decode_raw(data: bytes, slc: slice) -> bytes:
+        # return int.from_bytes(data[slc], "big")
+        # do the correct decoding later
         return data[slc]
 
 
@@ -26,15 +28,11 @@ class StrByteCodec(Type[bytes]):
     @staticmethod
     def decode_raw(data: bytes, slc: slice) -> bytes:
         item = data[slc]
-        if len(item) == 2:
-            # Method 1 makehex(int(item.hex()))
-            # Method 2: Split bytes, shift low byte, add to second byte
-            # split_bytes = [granularity_period[i:i + 1] for i in range(0, len(granularity_period), 1)]
-            return b"900"  # Hardcoded for now
+        # return item.decode("ascii").strip()
         return item
 
 
-class StrCP2Codec(Type[Union[str, bytes, datetime]]):
+class StrMetaCP2Codec(Type[Union[str, bytes, datetime]]):
     TYPECLASS = TypeClass.CONTEXT
     NATURE = [TypeNature.PRIMITIVE]
     TAG = 2
@@ -49,7 +47,7 @@ class StrCP2Codec(Type[Union[str, bytes, datetime]]):
         return chunk
 
 
-class StrCP3Codec(Type[str]):
+class StrMetaCP3Codec(Type[str]):
     TYPECLASS = TypeClass.CONTEXT
     NATURE = [TypeNature.PRIMITIVE]
     TAG = 3
@@ -61,6 +59,7 @@ class StrCP3Codec(Type[str]):
 
 
 class StrMetaCP4Codec(Type[Union[str, datetime]]):
+    # En Timestamp ass och e String
     TYPECLASS = TypeClass.CONTEXT
     NATURE = [TypeNature.PRIMITIVE]
     TAG = 4
@@ -73,7 +72,7 @@ class StrMetaCP4Codec(Type[Union[str, datetime]]):
         return chunk
 
 
-class SeqCodec(Type[Union[MeasFileHeader, NEId]]):
+class HeaderCodec(Type[Union[MeasFileHeader, NEId]]):
     TYPECLASS = TypeClass.CONTEXT
     NATURE = [TypeNature.CONSTRUCTED]
     TAG = 0
@@ -84,6 +83,7 @@ class SeqCodec(Type[Union[MeasFileHeader, NEId]]):
         step = slc.start
         while step < slc.stop:
             item, step = decode(data, step)
+            # print(item)
             items.append(item)
         if len(items) == 5:
             (
@@ -101,14 +101,61 @@ class SeqCodec(Type[Union[MeasFileHeader, NEId]]):
                 collection_begin_time_wrapped.value,
             )
         if len(items) == 2:
-            (ne_user_name_wrapped, ne_distinguished_name_wrapped) = items
+            # test
+            # (ne_user_name_wrapped, ne_distinguished_name_wrapped) = items
+            ne_user_name_wrapped = items[0]
+            ne_distinguished_name_wrapped = items[1]
             return NEId(
                 ne_user_name_wrapped.value, ne_distinguished_name_wrapped.value
             )
         raise DecodingUndefinedItemCount(f"{len(items)}")
 
 
+# class SeqListBytesCodec(Type[Union[List[bytes], MeasInfo]]): #<----- Original
+#     # Semantics: used for MeasResults (List[str]) and MeasInfo (Sequence of 4 items) - and MeasData?!
+#     TYPECLASS = TypeClass.CONTEXT
+#     NATURE = [TypeNature.CONSTRUCTED]
+#     TAG = 1
+
+#     @staticmethod
+#     def decode_raw(data: bytes, slc: slice) -> Union[List[bytes], MeasInfo]:
+#         chunk = data[slc]
+#         try:
+#             items, next = decode(chunk, 0, enforce_type=Sequence)
+#             (
+#                 meas_start_time_wrapped,
+#                 granularity_period_wrapped,
+#                 meas_types_wrapped,
+#                 meas_values_wrapped,
+#             ) = items
+#             return MeasInfo(
+#                 meas_start_time_wrapped.value,
+#                 granularity_period_wrapped.value,
+#                 meas_types_wrapped.value,
+#                 meas_values_wrapped.value,
+#             )
+#         except:
+
+#             # This is used for MeasData too which will result in a list of a list :-(
+#             # It seems that the rest of the data ("MeasInfo") is not being read
+
+#             result, next = decode(chunk, 0)
+#             meas_results_wrapped = [result.pythonize()]
+#             while next < len(chunk):
+#                 item, next = decode(
+#                     chunk,
+#                     next,
+#                 )
+#                 meas_results_wrapped.append(item.pythonize())
+
+#             return meas_results_wrapped
+
+#             # res, _ = decode(chunk)
+#             # return chunk
+
+
 class SeqListBytesCodec(Type[Union[List[bytes], MeasInfo, MeasData]]):
+    # Semantics: used for MeasResults (List[str]) and MeasInfo (Sequence of 4 items) - and MeasData?!
     TYPECLASS = TypeClass.CONTEXT
     NATURE = [TypeNature.CONSTRUCTED]
     TAG = 1
@@ -119,8 +166,14 @@ class SeqListBytesCodec(Type[Union[List[bytes], MeasInfo, MeasData]]):
     ) -> Union[List[bytes], MeasInfo, MeasData]:
         chunk = data[slc]
         try:
+            # items, next = decode(chunk, 0, enforce_type=Sequence)
             items, next = decode(data, slc.start, enforce_type=Sequence)
+            # print(f"ITEMS IN SEQENCE:{items}\n")
+
+            # if 18 >= len(items[0].value) > 14:
             if len(items) == 4 and isinstance(items[0], ByteCodec):
+                # pass
+
                 items = []
                 next_tlv = 0
                 while next_tlv < len(chunk):
@@ -140,25 +193,67 @@ class SeqListBytesCodec(Type[Union[List[bytes], MeasInfo, MeasData]]):
                         )
                     )
                 return items
+
+                # (
+                #     meas_start_time_wrapped,
+                #     granularity_period_wrapped,
+                #     meas_types_wrapped,
+                #     meas_values_wrapped,
+                # ) = items
+                # for list_item in meas_values_wrapped.value:
+                #     #print(list_item)
+                #     return MeasInfo(
+                #         meas_start_time_wrapped.value,
+                #         granularity_period_wrapped.value,
+                #         meas_types_wrapped.value,
+                #         list_item.value,
+                #     )
+
             if len(items) == 2:
+                # failhere
+                # print(len(items))
+
+                # print(f"\nexpected_ne_id:\n{items[0]}\n")
+                # print(f"\nexpected_meas_info:\n{items[1]}\n")
                 ne_id_wrapped, meas_info_wrapped = items
+
                 return MeasData(ne_id_wrapped.value, meas_info_wrapped.value)
+            # else:
+            #     return items
             raise DecodingUndefinedItemCount(
                 f"Caught undefined amout of items:{len(items)} with unkown type at items[0]: {type(items[0])}"
             )
+
         except:
-            item, next = decode(chunk, 0)
-            result = [item.pythonize()]
-            while next < len(chunk):
-                item, next = decode(
-                    chunk,
-                    next,
-                )
-                result.append(item.pythonize())
-            return result
+
+            dirtydebugswitch = 0
+            if dirtydebugswitch == 0:
+                item, next = decode(chunk, 0)
+                result = [item.pythonize()]
+                while next < len(chunk):
+                    item, next = decode(
+                        chunk,
+                        next,
+                    )
+                    result.append(item.pythonize())
+                # print("resultSTARTresultSTARTresultSTARTresultSTARTresultSTARTresultSTARTresultSTARTresultSTARTresultSTART")
+                # print("resultSTARTresultSTARTresultSTARTresultSTARTresultSTARTresultSTARTresultSTARTresultSTARTresultSTART")
+                # print(result)
+                # print("STOPresultSTOPresultSTOPresultSTOPresultSTOPresultSTOPresultSTOPresultSTOPresultSTOPresultSTOPresult")
+                # print("STOPresultSTOPresultSTOPresultSTOPresultSTOPresultSTOPresultSTOPresultSTOPresultSTOPresultSTOPresult")
+
+                return result
+            else:
+                items = []
+                next_tlv = 0
+                while next_tlv < len(chunk):
+                    item, next_tlv = decode(chunk, next_tlv)
+                    items.append(item)
+                return items
 
 
 class ListStrCodec(Type[List[str]]):
+    # Semantics: only used for "Measure types"
     TYPECLASS = TypeClass.CONTEXT
     NATURE = [TypeNature.CONSTRUCTED]
     TAG = 2
@@ -174,7 +269,7 @@ class ListStrCodec(Type[List[str]]):
         return types_wrapped
 
 
-class ListSeqCodec(Type[List[MeasValue]]):
+class ListBytesCodec(Type[List[MeasValue]]):
     TYPECLASS = TypeClass.CONTEXT
     NATURE = [TypeNature.CONSTRUCTED]
     TAG = 3
@@ -199,3 +294,15 @@ class ListSeqCodec(Type[List[MeasValue]]):
                 )
             )
         return items
+
+    #     sequence, _ = decode(data, slc.start)
+    #     (
+    #         obj_inst_id_wrapped,
+    #         meas_results_wrapped,
+    #         suspect_flag_wrapped,
+    #     ) = sequence
+    #     return MeasValue(
+    #         obj_inst_id_wrapped.value,
+    #         meas_results_wrapped.value,
+    #         suspect_flag_wrapped.value,
+    #     )
